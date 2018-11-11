413f9a5722965a553a96dddd6498d766e00b4dc1
ag113ate <shota.terui@beach.ocn.ne.jp>
2018-11-11 23:33:25 +0000

    Add a custom css definition

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,7 @@ module TwitterApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
